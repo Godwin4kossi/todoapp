e94b1a9fb0d737155d9f5d7e1b4798d23002af5b
Daniel Iheonu <iheonu6@gmail.com>
2025-02-05 17:15:10 +0100

    seperating components and stylings

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 import Todo from "./components/Todo"
+import Header from './components/Header'
+import "./App.css"
 
 function App() {
 
@@ -7,6 +9,7 @@ function App() {
   return (
     <>
       <div>
+        <Header />
        <Todo />
       </div>
     </>
